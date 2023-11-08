0f1ccc9f9d8ae3aa4f9bf5de0ef0ab1065a19da3
Patrick Kersten <info@zedero.nl>
2023-11-08 11:57:47 +0100

    remove reference

--- map editor/src/app/components/edit-dialog/edit-dialog.component.ts
+++ map editor/src/app/components/edit-dialog/edit-dialog.component.ts
@@ -1,6 +1,5 @@
 import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {tilesConfiguration} from "../../config";
 import {TilesService} from "../../service/tiles.service";
 import {ConfigService} from "../../service/config.service";
 
