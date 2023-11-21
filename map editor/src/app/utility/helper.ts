518b1e15aacd1172d0bb073cf608bec42933e649
Patrick Kersten <info@zedero.nl>
2023-11-21 15:01:58 +0100

    cleanup

--- map editor/src/app/utility/helper.ts
+++ map editor/src/app/utility/helper.ts
@@ -37,8 +37,7 @@ export class Helper {
   }
 
   distance(x1, x2, y1, y2) {
-    // calculate offset to cube and then convert that to distance
-
+    // calculate offset to cube
     var q = y1 - (x1 - (x1&1)) / 2;
     var r = x1;
     const s = -q-r;
@@ -47,26 +46,8 @@ export class Helper {
     var r2 = x2;
     const s2 = -q2-r2;
 
+    // cube coordinates to distance
     return (Math.abs(q - q2) + Math.abs(r - r2) + Math.abs(s - s2)) / 2;
-
-
-
-
-    const ax = x1 - this.floor2(y1);
-    const ay = x1 + this.ceil2(y1);
-    const bx = x2 - this.floor2(y2);
-    const by = x2 + this.ceil2(y2)
-    const dx = bx - ax;
-    const dy = by - ay;
-    if (Math.sign(dx) == Math.sign(dy)) {
-      return Math.max(Math.abs(dx), Math.abs(dy));
-    }
-    return Math.abs(dx) + Math.abs(dy);
-
-    // const a = x1 - x2;
-    // const b = y1 - y2;
-    // return Math.sqrt( a*a + b*b );
-    return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
   }
 
   getFurthestTwoPoints(tileList: any) {
@@ -122,26 +103,6 @@ export class Helper {
       }
     })
     return result;
-  }
-
-  HexDistance(p1X, p1Y,p2X, p2Y) {
-    const ax = p1X - this.floor2(p1Y);
-    const ay = p1X + this.ceil2(p1Y);
-    const bx = p2X - this.floor2(p2Y);
-    const by = p2X + this.ceil2(p2Y)
-    const dx = bx - ax;
-    const dy = by - ay;
-    if (Math.sign(dx) == Math.sign(dy)) {
-      return Math.max(Math.abs(dx), Math.abs(dy));
-    }
-    return Math.abs(dx) + Math.abs(dy);
-  }
-
-  private floor2(x: number) {
-    return ((x >= 0) ? (x >> 1) : (x - 1) / 2);
-  }
-  private ceil2(x: number) {
-    return ((x >= 0) ? ((x + 1) >> 1) : x / 2);
   }
 }
 
