72ac87e065ddd93d957c13e91b9db0534f68be36
Patrick Kersten <info@zedero.nl>
2024-04-17 14:38:57 +0200

    add downgrade option

--- fight emulator/src/app/app.component.ts
+++ fight emulator/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 export type Unit = {
   id: string;
@@ -8,8 +8,8 @@ export type Unit = {
   initiative: number;
   ranged: boolean;
   special: number[];
-  upgradeFrom?: string;
-  costs?: [number,number];
+  upgradeFrom: string;
+  costs: [number,number];
 }
 
 enum SPECIALS {
@@ -22,7 +22,7 @@ enum SPECIALS {
   "IGNORE_DAMAGE_FROM_SPECIALS", // TODO but not yet needed
   "DOUBLE_ATTACK_NON_ADJACENT", // TODO
   "REROLL_ZERO_ON_DICE",
-  "REROLL_ON_OTHER_SPACE",
+  "REROLL_ON_OTHER_SPACE", // TODO
 }
 
 @Component({
@@ -41,6 +41,8 @@ export class AppComponent {
     initiative: 4,
     ranged: false,
     special: [],
+    upgradeFrom: "",
+    costs: [2,0],
   },{
     id: 'TROGLODYTES_#PACK',
     attack: 3,
@@ -49,6 +51,8 @@ export class AppComponent {
     initiative: 5,
     ranged: false,
     special: [SPECIALS.IGNORE_PARALYSIS],
+    upgradeFrom: "TROGLODYTES",
+    costs: [3,0],
   },{
     id: 'HARPIES',
     attack: 2,
@@ -57,6 +61,8 @@ export class AppComponent {
     initiative: 6,
     ranged: false,
     special: [],
+    upgradeFrom: "",
+    costs: [3,0],
   },{
     id: 'HARPIES_#PACK',
     attack: 3,
@@ -65,6 +71,8 @@ export class AppComponent {
     initiative: 9,
     ranged: false,
     special: [SPECIALS.IGNORE_RETALIATION],
+    upgradeFrom: "HARPIES",
+    costs: [5,0],
   },{
     id: 'EVIL_EYES',
     attack: 3,
@@ -73,6 +81,8 @@ export class AppComponent {
     initiative: 5,
     ranged: true,
     special: [],
+    upgradeFrom: "",
+    costs: [4,0],
   },{
     id: 'EVIL_EYES_#PACK',
     attack: 3,
@@ -81,6 +91,8 @@ export class AppComponent {
     initiative: 7,
     ranged: true,
     special: [SPECIALS.IGNORE_COMBAT_PENALTY_ADJACENT],
+    upgradeFrom: "EVIL_EYES",
+    costs: [6,0],
   },{
     id: 'MEDUSAS',
     attack: 3,
@@ -89,6 +101,8 @@ export class AppComponent {
     initiative: 5,
     ranged: true,
     special: [SPECIALS.CHANCE_TO_PARALYZE_ON_RETALIATION],
+    upgradeFrom: "",
+    costs: [6,0],
   },{
     id: 'MEDUSAS_#PACK',
     attack: 4,
@@ -97,6 +111,8 @@ export class AppComponent {
     initiative: 6,
     ranged: true,
     special: [SPECIALS.PARALYZE_ON_RETALIATION, SPECIALS.IGNORE_COMBAT_PENALTY_ADJACENT],
+    upgradeFrom: "MEDUSAS",
+    costs: [12,0],
   },{
     id: 'MINOTAURS',
     attack: 4,
@@ -105,6 +121,8 @@ export class AppComponent {
     initiative: 6,
     ranged: false,
     special: [],
+    upgradeFrom: "",
+    costs: [8,0],
   },{
     id: 'MINOTAURS_#PACK',
     attack: 5,
@@ -113,6 +131,8 @@ export class AppComponent {
     initiative: 8,
     ranged: false,
     special: [],
+    upgradeFrom: "MINOTAURS",
+    costs: [14,0],
   },{
     id: 'MANTICORES',
     attack: 5,
@@ -121,6 +141,8 @@ export class AppComponent {
     initiative: 7,
     ranged: false,
     special: [],
+    upgradeFrom: "",
+    costs: [10,0],
   },{
     id: 'MANTICORES_#PACK',
     attack: 5,
@@ -129,6 +151,8 @@ export class AppComponent {
     initiative: 11,
     ranged: false,
     special: [SPECIALS.IGNORE_DEFENCE],
+    upgradeFrom: "MANTICORES",
+    costs: [18,1],
   },{
     id: 'BLACK_DRAGONS',
     attack: 6,
@@ -137,6 +161,8 @@ export class AppComponent {
     initiative: 11,
     ranged: false,
     special: [],
+    upgradeFrom: "",
+    costs: [19,2],
   },{
     id: 'BLACK_DRAGONS_#PACK',
     attack: 8,
@@ -145,6 +171,8 @@ export class AppComponent {
     initiative: 15,
     ranged: false,
     special: [SPECIALS.IGNORE_DAMAGE_FROM_SPECIALS],
+    upgradeFrom: "BLACK_DRAGONS",
+    costs: [33,2],
   },{
     id: 'HALBERDIERS',
     attack: 2,
@@ -153,6 +181,8 @@ export class AppComponent {
     initiative: 4,
     ranged: false,
     special: [],
+    upgradeFrom: "",
+    costs: [2,0],
   },{
     id: 'HALBERDIERS_#PACK',
     attack: 3,
@@ -161,6 +191,8 @@ export class AppComponent {
     initiative: 5,
     ranged: false,
     special: [],
+    upgradeFrom: "HALBERDIERS",
+    costs: [3,0],
   },{
     id: 'MARKSMEN',
     attack: 2,
@@ -169,6 +201,8 @@ export class AppComponent {
     initiative: 4,
     ranged: true,
     special: [],
+    upgradeFrom: "",
+    costs: [3,0],
   },{
     id: 'MARKSMEN_#PACK',
     attack: 2,
@@ -177,6 +211,8 @@ export class AppComponent {
     initiative: 6,
     ranged: true,
     special: [SPECIALS.DOUBLE_ATTACK_NON_ADJACENT],
+    upgradeFrom: "MARKSMEN",
+    costs: [5,0],
   },{
     id: 'GRIFFINS',
     attack: 2,
@@ -185,6 +221,8 @@ export class AppComponent {
     initiative: 6,
     ranged: false,
     special: [],
+    upgradeFrom: "",
+    costs: [4,0],
   },{
     id: 'GRIFFINS_#PACK',
     attack: 3,
@@ -193,6 +231,8 @@ export class AppComponent {
     initiative: 9,
     ranged: false,
     special: [],
+    upgradeFrom: "GRIFFINS",
+    costs: [6,0],
   },{
     id: 'CRUSADERS',
     attack: 3,
@@ -201,6 +241,8 @@ export class AppComponent {
     initiative: 5,
     ranged: false,
     special: [],
+    upgradeFrom: "",
+    costs: [6,0],
   },{
     id: 'CRUSADERS_#PACK',
     attack: 4,
@@ -209,6 +251,8 @@ export class AppComponent {
     initiative: 6,
     ranged: false,
     special: [SPECIALS.REROLL_ZERO_ON_DICE],
+    upgradeFrom: "CRUSADERS",
+    costs: [10,0],
   },{
     id: 'ZEALOTS',
     attack: 3,
@@ -217,6 +261,8 @@ export class AppComponent {
     initiative: 4,
     ranged: true,
     special: [],
+    upgradeFrom: "",
+    costs: [8,0],
   },{
     id: 'ZEALOTS_#PACK',
     attack: 4,
@@ -225,6 +271,8 @@ export class AppComponent {
     initiative: 7,
     ranged: true,
     special: [SPECIALS.IGNORE_COMBAT_PENALTY_ADJACENT],
+    upgradeFrom: "ZEALOTS",
+    costs: [12,0],
   },{
     id: 'CHAMPIONS',
     attack: 5,
@@ -233,6 +281,8 @@ export class AppComponent {
     initiative: 7,
     ranged: false,
     special: [],
+    upgradeFrom: "",
+    costs: [12,0],
   },{
     id: 'CHAMPIONS_#PACK',
     attack: 6,
@@ -241,6 +291,8 @@ export class AppComponent {
     initiative: 9,
     ranged: false,
     special: [SPECIALS.REROLL_ON_OTHER_SPACE],
+    upgradeFrom: "CHAMPIONS",
+    costs: [20,1],
   },{
     id: 'ARCHANGELS',
     attack: 6,
@@ -249,6 +301,8 @@ export class AppComponent {
     initiative: 12,
     ranged: false,
     special: [],
+    upgradeFrom: "",
+    costs: [20,1],
   },{
     id: 'ARCHANGELS_#PACK',
     attack: 7,
@@ -257,14 +311,16 @@ export class AppComponent {
     initiative: 18,
     ranged: false,
     special: [],
+    upgradeFrom: "ARCHANGELS",
+    costs: [30,2]
   }]
   constructor() {
-
+    console.log(this.units)
   }
 
   public start() {
     // test match
-    // this.doBattle(this.units[5], this.units[3]);
+    // this.doBattle(this.units[26], this.units[25]);
     // return;
 
     const matches = this.setupBattleMatches();
@@ -331,13 +387,15 @@ export class AppComponent {
     }
 
     if (attackerWon > defenderWon) {
-      console.log(`${this.name(attacker.id)} has won (${attackerWon}/${itterations}) agains ${this.name(defender.id)}`)
+      // console.log(`${this.name(attacker.id)} has won (${attackerWon}/${itterations}) agains ${this.name(defender.id)}`)
+      // console.log('-----------------------------------')
       return {
         winner: attacker,
         percentage: Math.round((attackerWon/itterations) * 100)
       };
     } else {
-      console.log(`${this.name(defender.id)} has won (${defenderWon}/${itterations}) agains ${this.name(attacker.id)}`)
+      // console.log(`${this.name(defender.id)} has won (${defenderWon}/${itterations}) agains ${this.name(attacker.id)}`)
+      // console.log('-----------------------------------')
       return {
         winner: defender,
         percentage: Math.round((defenderWon/itterations) * 100)
@@ -356,34 +414,72 @@ export class AppComponent {
 
   private startCombat(attacker: Unit, defender: Unit, isAdjacent = false): Unit {
     isAdjacent = this.checkAdjacency(attacker, isAdjacent);
-    this.doDamage(attacker, defender, isAdjacent);
+    this.doDamage(attacker, defender, isAdjacent, false);
     if (this.isDead(defender)) {
-      return attacker;
+      const downgrade = this.findDowngrade(defender)
+      if (!downgrade) {
+        return attacker;
+      } else {
+        defender = this.doDowngrade(defender, downgrade);
+        if (this.isDead(defender)) {
+          return attacker;
+        }
+      }
     }
 
     if (this.hasSkill(attacker, SPECIALS.IGNORE_RETALIATION) || (attacker.ranged && !isAdjacent)) {
       // Don't retaliate if the attacker ignores it
       // or if the attacker is ranged but not adjacent to the defender
     } else {
-      this.doDamage(defender, attacker, isAdjacent);
+      // console.log(defender.id, 'RETALLIATE')
+      this.doDamage(defender, attacker, isAdjacent, true);
     }
 
     if (this.isDead(attacker)) {
-      return defender;
+      const downgrade = this.findDowngrade(attacker)
+      if (!downgrade) {
+        return defender;
+      } else {
+        attacker = this.doDowngrade(attacker, downgrade);
+        if (this.isDead(attacker)) {
+          return defender;
+        }
+      }
     }
     return this.startCombat(defender, attacker, isAdjacent);
+  }
+
+  private doDowngrade(upgrade: Unit, downgrade: Unit) {
+    upgrade.health += downgrade.health; // set new health but adjust for negative damage.
+    upgrade.attack = downgrade.attack;
+    upgrade.defence = downgrade.defence;
+    upgrade.initiative = downgrade.initiative;
+    upgrade.ranged = downgrade.ranged;
+    upgrade.special = downgrade.special;
+    upgrade.upgradeFrom = downgrade.upgradeFrom;
+    // console.log(`${upgrade.id} has been downgraded to ${downgrade.id}(${upgrade.health})`)
+    return upgrade;
+  }
+
+  private findDowngrade(unit: Unit) {
+    return this.units.find((un) => {
+      return un.id === unit.upgradeFrom
+    });
   }
 
   private isDead(target: Unit) {
     return target.health <= 0;
   }
 
-  private doDamage(source: Unit, target: Unit, isAdjacent: boolean) {
+  private doDamage(source: Unit, target: Unit, isAdjacent: boolean, retalliation: boolean) {
     const isRangedVsRanged = source.ranged && target.ranged && !isAdjacent;
     const isRangedVsAdjacent = source.ranged && isAdjacent;
     const combatPenalty = isRangedVsRanged || (isRangedVsAdjacent && !this.hasSkill(source, SPECIALS.IGNORE_COMBAT_PENALTY_ADJACENT));
     const attackRoll = () => {
-      return this.attackRoll(combatPenalty, this.hasSkill(source, SPECIALS.REROLL_ZERO_ON_DICE));
+      return this.attackRoll(
+        combatPenalty,
+        this.hasSkill(source, SPECIALS.REROLL_ZERO_ON_DICE),
+        (this.hasSkill(source, SPECIALS.REROLL_ON_OTHER_SPACE) && !retalliation));
     };
 
     const attack = () => {
@@ -405,15 +501,22 @@ export class AppComponent {
 
     const damage = attack() - targetDefence();
     target.health -= damage;
-    //console.log(`${this.name(source.id)} deals ${damage} damage to ${this.name(target.id)} (${combatPenalty})`)
+    // console.log(`${this.name(source.id)} deals ${damage} damage to ${this.name(target.id)}(${target.health}) (${combatPenalty})`)
   }
 
-  private attackRoll(combatPenalty: boolean, reRollOnZero: boolean) {
+  private attackRoll(combatPenalty: boolean, reRollOnZero: boolean, reRollOnNewSpace: boolean) {
     if (reRollOnZero) {
       return Math.random() < 0.5 ? -1 : 1;
     }
     const roll = function () {
       return Math.floor(Math.random() * 3) - 1
+    }
+    if (reRollOnNewSpace) {
+      const diceRoll = roll();
+      if (diceRoll === -1) {
+        return roll()
+      }
+      return diceRoll;
     }
     if (combatPenalty) {
       return Math.min(roll(),roll());
